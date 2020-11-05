edeff70832be8168322f167bcb3acd0a0904d34e
whoamineveminds <whoamineveminds@gmail.com>
2020-11-05 20:58:21 +0700

    heroku pls start

--- app/Models/Plans.php
+++ app/Models/Plans.php
@@ -17,12 +17,11 @@ class Plans extends Model
         return $this->belongsTo('App\Models\Lists', 'list_id');
     }
 
-    public static function boot()
+    protected static function booted()
     {
-        parent::boot();
-        self::creating(function ($model) {
-            if (!$model->complete) {
-                $model->getList->increment('undone');
+        static::created(function ($user) {
+            if (!$user->complete) {
+                $user->getList->increment('undone');
             }
         });
     }
